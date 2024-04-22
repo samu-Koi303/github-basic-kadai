4f49d1ada6d2d92f6ee3c605d13bec791b732a5d
samu-Koi303 <167265512+samu-Koi303@users.noreply.github.com>
2024-04-22 10:46:17 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,13 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 extends Dictionary_Chapter21 {
+
+	public static void main(String[] args) {
+
+		Dictionary_Chapter21 Dic = new Dictionary_Chapter21();
+
+		Dic.searchWord();
+
+	}
+
+}
